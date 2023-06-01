5680d4ec997113de8f49121f879d22af6f5087ca
mwjng <wmw04087@gmail.com>
2023-06-01 17:14:49 +0900

    Update RegisterController.java

--- src/main/java/com/example/demo/controller/RegisterController.java
+++ src/main/java/com/example/demo/controller/RegisterController.java
@@ -22,7 +22,7 @@ public class RegisterController {
 
     @PostMapping("/corporate")
     public ResponseEntity<InfoDto> createAllCorporate(@RequestPart("data") InfoDto dto, @RequestPart("image") MultipartFile[] images, @RequestPart("seal") MultipartFile[] seals) {
-        InfoDto infoDto = infoService.createCorp(dto, images, "image", seals, "seal");;
+        InfoDto infoDto = infoService.createCorp(dto, images, "image", seals, "seal");
         return ResponseEntity.status(HttpStatus.OK).body(infoDto);
     }
 }
